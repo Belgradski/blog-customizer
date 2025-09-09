77fad42f8ba43ca252a499fd562bd526c3cb3573
Alexey Belgradski <Alexeybelgradski@yandex.ru>
2025-09-09 17:14:30 +0300

    feature: create app component

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,58 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from '../../ui/text/Text';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from '../../constants/articleProps';
+
+import { useState, FormEvent } from 'react';
+
+export type ArticleParamsFormProps = {
+	setAppState: (value: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const { setAppState } = props;
+
+	const [isOpened, setIsOpened] = useState<boolean>(false);
+
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setAppState(formState);
+	};
+
+	const handleReset = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setFormState(defaultArticleState);
+		setAppState(formState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpened}
+				onClick={() => setIsOpened((currentIsOpened) => !currentIsOpened)}
+			/>
+			<div
+				onClick={() => setIsOpened(false)}
+				className={clsx(styles.overlay, isOpened && styles.overlay_open)}></div>
+			<aside
+				className={clsx(styles.container, isOpened && styles.container_open)}>
+				<form
+					onSubmit={handleSubmit}
+					onReset={handleReset}
+					className={styles.form}>
+					<Text uppercase={true} weight={800} size={31}>
+						Задайте параметры
+					</Text>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
